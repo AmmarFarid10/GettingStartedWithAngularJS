8c5c8b3074ba2babd196a3e036d1701b0960944f
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2021-10-01 17:07:05 +0500

    sum
    ry

--- js/services/cacheFactoryService.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Rahul_Sahay on 7/21/2015.
- */
-
-'use strict';
-
-moviesApp.factory('cacheFactory',function($cacheFactory){
-  return $cacheFactory('cacheFactory',{capacity:5})
-});
